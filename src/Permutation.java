b2ff415f4212bf34f62c0687a6812f0b04633d78
HJS <407410014@qq.com>
2017-10-14 15:37:33 +0800

    finish Permutation.java

--- src/Permutation.java
+++ src/Permutation.java
@@ -1,6 +1,17 @@
-
+import edu.princeton.cs.algs4.StdIn;
 
 public class Permutation {
     public static void main(String[] args)
-    {}
+    {
+        int count = StdIn.readInt();
+        RandomizedQueue<String> rq = new RandomizedQueue<>();
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            rq.enqueue(s);
+            if (s == null)  break;
+        }
+        for(int i = 0; i < count; i++) {
+            System.out.println(rq.dequeue());
+        }
+    }
  }
